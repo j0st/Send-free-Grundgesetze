609be65a9f0bcab012325c51f7ea032808d9805a
j0st <jost.markus@gmx.de>
2022-12-28 12:28:13 +0100

    added config

--- main.py
+++ main.py
@@ -4,48 +4,43 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import configparser
 
+config = configparser.ConfigParser()
+config.read('adress.cfg')
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.headless = True
+#options.headless = True
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
 
 url = 'https://www.bpb.de/shop/buecher/grundgesetz/34367/grundgesetz-fuer-die-bundesrepublik-deutschland/'
-
 amount = 6
-first_name = 'Test'
-last_name = 'Test'
-street = 'Teststr.' 
-street_number = '1'
-postal_code = '11111'
-city = 'Test'
-email = 'test@fyii.de'
 
 # start page
 driver.get(url)
 driver.find_element(By.XPATH, '/html/body/div/main/article/div/div[2]/section[1]/section/form/div/div[1]/div/input').send_keys(Keys.BACKSPACE)
 driver.find_element(By.XPATH, '/html/body/div/main/article/div/div[2]/section[1]/section/form/div/div[1]/div/input').send_keys(amount)
 driver.find_element(By.XPATH, '//*[@id="app"]/main/article/div/div[2]/section[1]/section/form/div/div[2]/button').click()
-time.sleep(5)
+time.sleep(2)
 
 # added to cart page
 driver.find_element(By.XPATH, '//*[@id="app"]/div[4]/div/div/article/footer/a').click()
-time.sleep(5)
+time.sleep(2)
 
 # to checkout
 driver.find_element(By.XPATH, '//*[@id="app"]/main/section/div[2]/form/div/div[2]/button').click()
-time.sleep(5)
+time.sleep(2)
 
 # adress page
-driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[1]/div/input').send_keys(first_name)
-driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[2]/div/input').send_keys(last_name)
-driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[4]/div/input').send_keys(street)
-driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[5]/div/input').send_keys(street_number)
-driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[6]/div/input').send_keys(postal_code)
-driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[7]/div/input').send_keys(city)
-driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[9]/div/input').send_keys(email)
+driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[1]/div/input').send_keys(config.get('ADRESS','FIRST_NAME'))
+driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[2]/div/input').send_keys(config.get('ADRESS','LAST_NAME'))
+driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[4]/div/input').send_keys(config.get('ADRESS','STREET'))
+driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[5]/div/input').send_keys(config.get('ADRESS','STREET_NUMBER'))
+driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[6]/div/input').send_keys(config.get('ADRESS','POSTALCODE'))
+driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[7]/div/input').send_keys(config.get('ADRESS','CITY'))
+driver.find_element(By.XPATH, '/html/body/div/main/div[6]/div[2]/div/form/div[2]/fieldset/div/div[9]/div/input').send_keys(config.get('ADRESS','EMAIL'))
 driver.find_element(By.XPATH, '//*[@id="app"]/main/div[6]/div[2]/div/form/div[3]/button').click()
 
 # submit page
